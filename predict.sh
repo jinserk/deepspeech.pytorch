bd33d5df8268258da228eb2f171f2f7799d00dd5
Jinserk Baik <jinserk.baik@gmail.com>
2017-09-06 15:37:17 -0500

    change predict.py to transcribe.py in predict.sh
    - add --cuda option

--- predict.sh
+++ predict.sh
@@ -2,23 +2,27 @@
 
 model="models/20170829/deepspeech_10.pth.tar"
 decoder="beam"
+cuda="yes"
 
 . parse_options.sh
 
 wav=$1
+[ "$cuda" == "yes" ] && cuda_opt="--cuda" || cuda_opt=""
 
 if [ "$decoder" == "beam" ]; then
-	python predict.py \
+	python transcribe.py \
 		--model_path $model \
 		--decoder $decoder \
 		--beam_width 20 \
 		--lm_path lm/lm.binary \
 		--trie_path lm/lm.trie \
 		--lm_alpha 0.8 \
+		$cuda_opt \
 		--audio_path $wav
 else
-	python predict.py \
+	python transcribe.py \
 		--model_path $model \
 		--decoder $decoder \
+		$cuda_opt \
 		--audio_path $wav
 fi
